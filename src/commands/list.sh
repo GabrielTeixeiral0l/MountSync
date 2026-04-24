fe42f4451c59dc130243d4cdd99d319fc2e390c0
Gabriel Teixeira <teixeiramarvao@gmail.com>
2026-04-24 22:35:30 +0100

    feat(commands): implement list command

---
+++ src/commands/list.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+list_callback() {
+    local local_rel=$1
+    echo "- $local_rel"
+}
+
+cmd_list() {
+    if [ ! -f "$MAP_FILE" ]; then
+        echo "No items are currently being managed by MountSync."
+        return 0
+    fi
+
+    echo "Items managed by MountSync:"
+    foreach_mapping list_callback
+}
